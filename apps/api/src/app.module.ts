d1ba3345b7c4c081c60555d671e70112cbb4cf23
Efraim <skaylet2007@gmail.com>
2026-03-10 00:14:00 +0100

    fix tests 8

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -18,19 +18,11 @@ const isTest = process.env.NODE_ENV === 'test';
 @Module({
   imports: [
     ConfigModule,
-    ThrottlerModule.forRoot(
-      isTest
-        ? [
-            { name: 'short', ttl: 1000, limit: 1000 },
-            { name: 'medium', ttl: 10000, limit: 1000 },
-            { name: 'long', ttl: 60000, limit: 1000 },
-          ]
-        : [
-            { name: 'short', ttl: 1000, limit: 3 },
-            { name: 'medium', ttl: 10000, limit: 20 },
-            { name: 'long', ttl: 60000, limit: 100 },
-          ],
-    ),
+    ThrottlerModule.forRoot([
+      { name: 'short', ttl: 1000, limit: 3 },
+      { name: 'medium', ttl: 10000, limit: 20 },
+      { name: 'long', ttl: 60000, limit: 100 },
+    ]),
     PrismaModule,
     AuthModule,
     BlockModule,
@@ -43,7 +35,9 @@ const isTest = process.env.NODE_ENV === 'test';
   providers: [
     AppService,
     { provide: APP_GUARD, useClass: JwtAuthGuard },
-    { provide: APP_GUARD, useClass: ThrottlerGuard },
+    ...(isTest
+      ? []
+      : [{ provide: APP_GUARD, useClass: ThrottlerGuard }]),
   ],
 })
 export class AppModule {}
